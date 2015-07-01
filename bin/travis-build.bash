172ff39ac814a5b19261077f15a3f601191cc74e
Brook Elgie <specialbrew@gmail.com>
2015-07-01 10:49:01 +0100

    Set up Travis

--- bin/travis-build.bash
+++ bin/travis-build.bash
@@ -10,7 +10,7 @@ sudo apt-get install postgresql-$PGVERSION solr-jetty libcommons-fileupload-java
 echo "Installing CKAN and its Python dependencies..."
 git clone https://github.com/ckan/ckan
 cd ckan
-git checkout release-v2.2
+git checkout 2510-iuploader-interface
 python setup.py develop
 pip install -r requirements.txt --allow-all-external
 pip install -r dev-requirements.txt --allow-all-external
