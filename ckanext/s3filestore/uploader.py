e347e5633ec467309f51a7943809d4a46a1f2322
Jovanka Gulicoska <jovanka.gulicoska@keitaro.com>
2016-12-21 23:48:25 +0100

    Skip session

--- ckanext/s3filestore/uploader.py
+++ ckanext/s3filestore/uploader.py
@@ -39,11 +39,11 @@ class BaseS3Uploader(object):
         region = config.get('ckanext.s3filestore.region_name')
 
         # make s3 connection
-        session = boto3.session.Session('s3',
-                    aws_access_key_id=p_key,
-                    aws_secret_access_key=s_key,
-                    region_name=region,)
-        S3_conn = session.resource('s3')
+        # session = boto3.session.Session('s3',
+        #             aws_access_key_id=p_key,
+        #             aws_secret_access_key=s_key,
+        #             region_name=region,)
+        S3_conn = boto3.resource('s3')
 
         # make sure bucket exists and that we can access
         try:
