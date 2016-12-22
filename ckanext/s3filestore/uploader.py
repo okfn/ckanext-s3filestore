4f28fea9195890367d39f8623f973a46173b7fb7
Jovanka Gulicoska <jovanka.gulicoska@keitaro.com>
2016-12-22 11:23:19 +0100

    change resource with client

--- ckanext/s3filestore/uploader.py
+++ ckanext/s3filestore/uploader.py
@@ -80,7 +80,7 @@ class BaseS3Uploader(object):
 
         session = boto3.session.Session(region_name='eu-central-1')
 
-        s3 = session.resource('s3', config= boto3.session.Config(signature_version='s3v4'))
+        s3 = session.client('s3', config= boto3.session.Config(signature_version='s3v4'))
         transfer = boto3.s3.transfer.S3Transfer(s3)
         #obj = s3.Object(self.bucket.name, upload_file)
         try:
