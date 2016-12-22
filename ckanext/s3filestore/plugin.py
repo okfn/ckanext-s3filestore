e53fb828df15b994d9ef4fa3fa90edef19e28ab0
Jovanka Gulicoska <jovanka.gulicoska@keitaro.com>
2016-12-22 13:12:54 +0100

    add region in config

--- ckanext/s3filestore/plugin.py
+++ ckanext/s3filestore/plugin.py
@@ -26,7 +26,7 @@ class S3FileStorePlugin(plugins.SingletonPlugin):
             'ckanext.s3filestore.aws_access_key_id',
             'ckanext.s3filestore.aws_secret_access_key',
             'ckanext.s3filestore.aws_bucket_name',
-            'ckanext.s3filestore.region_name'
+            'ckanext.s3filestore.region_name',
         )
         for option in config_options:
             if not config.get(option, None):
