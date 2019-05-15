52a56012df983e96693b40bb186afc75049f1605
bstutsky <bstutsky@basispath.com>
2019-05-15 06:49:32 -0400

    Support for AMI Roles

--- ckanext/s3filestore/plugin.py
+++ ckanext/s3filestore/plugin.py
@@ -22,15 +22,18 @@ class S3FileStorePlugin(plugins.SingletonPlugin):
         # Certain config options must exists for the plugin to work. Raise an
         # exception if they're missing.
         missing_config = "{0} is not configured. Please amend your .ini file."
-        config_options = (
-            'ckanext.s3filestore.aws_access_key_id',
-            'ckanext.s3filestore.aws_secret_access_key',
+
+        required_options = (
             'ckanext.s3filestore.aws_bucket_name',
             'ckanext.s3filestore.region_name',
             'ckanext.s3filestore.signature_version',
             'ckanext.s3filestore.host_name'
         )
-        for option in config_options:
+        if not config.get('ckanext.s3filestore.aws_use_ami_role'):
+            required_options += ('ckanext.s3filestore.aws_access_key_id',
+                                 'ckanext.s3filestore.aws_secret_access_key')
+
+        for option in required_options:
             if not config.get(option, None):
                 raise RuntimeError(missing_config.format(option))
 
