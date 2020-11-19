8aea4504750f6ae0c4c5524bec704f62b4bebfb1
duskobogdanovski <dule.bogdanovski@gmail.com>
2020-11-19 16:55:42 +0100

    Fix typo

--- bin/travis-build.bash
+++ bin/travis-build.bash
@@ -46,7 +46,7 @@ echo "Setting up Solr..."
 docker run --name ckan-solr -p 8983:8983 -d openknowledge/ckan-solr-dev:$CKANVERSION
 
 echo "Setting up Minio..."
-docker run -it -d --name minio --publish 9000:9000 --env MINIO_ACCESS_KEY="test-access-key" --env MINIO_SECRET_KEY="test-secret-key" --env MINIO_REGION_NAME='us-east-1' --env MINIO_DEFAULT_BUCKETS='test-bucket' bitnami/minio:latestdocker run -it --rm --name minio --publish 9000:9000 --env MINIO_ACCESS_KEY="test-access-key" --env MINIO_SECRET_KEY="test-secret-key" --env MINIO_REGION_NAME='us-east-1' --env MINIO_DEFAULT_BUCKETS='test-bucket' bitnami/minio:latest
+docker run -it -d --name minio --publish 9000:9000 --env MINIO_ACCESS_KEY="test-access-key" --env MINIO_SECRET_KEY="test-secret-key" --env MINIO_REGION_NAME='us-east-1' --env MINIO_DEFAULT_BUCKETS='test-bucket' bitnami/minio:latest
 
 echo "Setting up Postgres..."
 export PG_VERSION="$(pg_lsclusters | grep online | awk '{print $1}')"
